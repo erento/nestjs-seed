aa8d968deb0597a4629407b58fd42d571dafc3bb
Florian Topf <florian.topf@gmail.com>
2018-08-03 10:33:47 +0200

    feature: adds dummy route for favicon

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
-import {Controller, Post} from '@nestjs/common';
+import {Controller, Get, Post} from '@nestjs/common';
 import {Auth} from './common/guards/authorization.decorator';
 import {AuthorizationType} from './common/guards/authorization.type';
 
 @Controller()
 export class AppController {
+    @Get('favicon.ico')
+    public favicon (): void {}
+
     @Post('authorization-checker')
     @Auth(AuthorizationType.service)
     public authorizationChecker (): any {
